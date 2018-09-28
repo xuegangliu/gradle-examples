a03180e62c19b819229db4285af70b0f8223d8cd
Max <lxghoney@163.com>
2018-09-28 15:03:07 +0800

    gradle init helloworld

---
+++ src/main/java/org/lxg/gradle/sample/HelloWorld.java
@@ -0,0 +1,13 @@
+package org.lxg.gradle.sample;
+
+/**
+ * @author: xuegangliu
+ * @date: 9/28/2018 2:17 PM
+ * @DES:
+ * @version: v1.0
+ */
+public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("HelloWorld!");
+    }
+}
